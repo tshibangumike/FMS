c84af67b1f2c68b2d2bee68c8c7458a3692ec67f
MikeBukasa <tshibangumike@gmail.com>
2018-04-27 21:59:10 +0200

    1st commit

---
+++ fms.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="fms.Web.MvcApplication" Language="C#" %>
---
+++ fms.Web/assets/funeral/FuneralController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fms.Web.assets.funeral
+{
+    public class FuneralController : Controller
+    {
+        // GET: Funeral
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
